4362bc4412c5a6c2cbc113cb6ed0678b4f29cf78
clintt-09 <aisosaclinton09@gmail.com>
2025-02-20 00:33:23 +0100

    resize observer polyfill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 import Header from '@/app/ui/components/Header'
+import ResizeObserver from "resize-observer-polyfill"
 import Footer from '@/app/ui/components/Footer'
 import CulturioContent from './ui/components/CulturioContent'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+if (typeof window !== 'undefined'){
+  window.ResizeObserver = ResizeObserver;
+}
 
 export default function Home() {
     const queryClient = new QueryClient()
