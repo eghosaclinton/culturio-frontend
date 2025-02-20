79b9e764f3f159a6b1783ddac7a38b57eb648cdb
clintt-09 <aisosaclinton09@gmail.com>
2025-02-20 01:43:23 +0100

    removed some packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
-// import ResizeObserverPolyfill from './ui/components/ResizeObserver'
 import './globals.css'
 
 // const geistSans = Geist({
